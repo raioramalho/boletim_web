2b69544d9ce912f1951703041761827f495038d8
raioramalho <ramalho.sit@gmail.com>
2023-10-23 13:50:56 -0300

    Marca dagua

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <span className="flex flex-row justify-center text-gray-900">Alan Ramalho - ramalho.sit@gmail.com</span>
     </html>
   )
 }
